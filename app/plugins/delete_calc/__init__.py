4eb5883ddd1afb57546b19033921b836012da2d2
root <root@DESKTOP-1E7E5O2>
2024-10-26 16:02:27 -0400

    fixed delete_calc

--- app/plugins/delete_calc/__init__.py
+++ app/plugins/delete_calc/__init__.py
@@ -3,14 +3,14 @@ from calculator.calculations import Calculations
 import logging
 
 class DeleteCalculationCommand(Command):
-    def execute(self, a, b=None):
-        self.a = a 
+    def execute(self, a, b):
+        self.b = b
         try:
-            logging.info(f"Deleted record: {Calculations.delete_calculation(int(a))}")
-            print(f"Record with id {a} deleted.")
+            logging.info(f"Deleted record: {Calculations.delete_calculation(int(b))}")
+            print(f"Record with id {b} deleted.")
         except ValueError:
             logging.error("Usage: delete_calc <id>")
             print("Usage: delete_calc <id>")
         except KeyError:
-            logging.error(f"Calculation with id {int(a)} does not exist")
-            print(f"Calculation with id {int(a)} does not exist")
+            logging.error(f"Calculation with id {int(b)} does not exist")
+            print(f"Calculation with id {int(b)} does not exist")
